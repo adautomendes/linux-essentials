c8a14810497cf7805da5cdb4df5a79d57cbb4b4b
Adauto Mendes Bernardes Júnior <adauto.inatel@gmail.com>
2017-09-27 16:56:53 -0300

    Updated WPS Office

--- linux-essentials.sh
+++ linux-essentials.sh
@@ -72,7 +72,7 @@ sudo apt-get clean
 sudo apt-get autoremove
 
 echo "Instalar WPS Office"
-cd && wget -O wps-office.deb http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb
+cd && wget -O wps-office.deb http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb
 sudo dpkg -i wps-office.deb
 sudo apt-get -f install && rm wps-office.deb
 wget -O web-office-fonts.deb http://kdl.cc.ksosoft.com/wps-community/download/fonts/wps-office-fonts_1.0_all.deb
