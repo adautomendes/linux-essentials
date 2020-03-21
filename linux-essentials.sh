cb425afb0a928023dd5544f8e0fb10df14263438
Adauto Mendes <adauto.inatel@gmail.com>
2020-03-21 16:09:08 -0300

    Atualizando versões

--- linux-essentials.sh
+++ linux-essentials.sh
@@ -15,16 +15,15 @@ echo "Instalar Chromium Browser"
 sudo apt-get install -y chromium-browser
 
 echo "Instalar Opera Browser"
-wget http://download1.operacdn.com/pub/opera/desktop/50.0.2762.67/linux/opera-stable_50.0.2762.67_amd64.deb
-sudo dpkg -i opera-stable_50.0.2762.67_amd64.deb
+wget https://download1.operacdn.com/pub/opera/desktop/67.0.3575.97/linux/opera-stable_67.0.3575.97_amd64.deb
+sudo dpkg -i opera-stable_67.0.3575.97_amd64.deb
 sudo apt-get -f install
-rm opera-stable_50.0.2762.67_amd64.deb
+rm opera-stable_67.0.3575.97_amd64.deb
 
 echo "Instalar Spotify"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 echo "Instalar Git"
 sudo apt-get install -y git
@@ -37,10 +36,7 @@ sudo apt-get update
 sudo apt-get install -y sublime-text
 
 echo "Instalar VSCode"
-wget https://az764295.vo.msecnd.net/stable/41abd21afdf7424c89319ee7cb0445cc6f376959/code_1.15.1-1502903936_amd64.deb
-sudo dpkg -i code_1.15.1-1502903936_amd64.deb
-sudo apt-get install -f
-rm code_1.15.1-1502903936_amd64.deb
+sudo snap install code --classic
 
 echo "Instalar lamp"
 sudo apt-get update
@@ -48,16 +44,16 @@ sudo apt-get install -y lamp-server^
 sudo apt-get update
 
 echo "Instalar phpmyadmin"
-wget https://files.phpmyadmin.net/phpMyAdmin/4.6.6/phpMyAdmin-4.6.6-all-languages.tar.bz2
-tar xf phpMyAdmin-4.6.6-all-languages.tar.bz2
-sudo mv phpMyAdmin-4.6.6-all-languages /var/www/html/phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar xf phpMyAdmin-5.0.2-all-languages.tar.gz
+sudo mv phpMyAdmin-5.0.2-all-languages /var/www/html/phpmyadmin
 sudo chmod -R 777 /var/www/html/phpmyadmin
 sudo apt-get install -y php-mbstring
 sudo service apache2 restart
-rm phpMyAdmin-4.6.6-all-languages.tar.bz2
+rm phpMyAdmin-5.0.2-all-languages.tar.gz
 
 echo "Instalar Node.js"
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
@@ -78,12 +74,9 @@ sudo apt-get clean
 sudo apt-get autoremove
 
 echo "Instalar WPS Office"
-cd && wget -O wps-office.deb http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb
+cd && wget -O wps-office.deb http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9126/wps-office_11.1.0.9126.XA_amd64.deb
 sudo dpkg -i wps-office.deb
 sudo apt-get -f install && rm wps-office.deb
-wget -O web-office-fonts.deb http://kdl.cc.ksosoft.com/wps-community/download/fonts/wps-office-fonts_1.0_all.deb
-sudo dpkg -i web-office-fonts.deb
-sudo apt-get -f install && rm web-office-fonts.deb
 
 echo "Instalar fontes do Windows"
 sudo apt install ttf-mscorefonts-installer
@@ -98,9 +91,5 @@ sudo mkfontdir
 cd ..
 fc-cache
 
-echo "Instalar Dropbox"
-wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-sudo dpkg -i download\?dl\=packages%2Fubuntu%2Fdropbox_2015.10.28_amd64.deb
-
-sudo shutdown -r 0
+echo "Script finalizado..."
+echo "Reinicie a sistema!"
