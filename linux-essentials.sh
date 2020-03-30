dc92771e188b09bd44097b20572356bc2bb66a99
Adauto Mendes Bernardes Júnior <adauto.inatel@gmail.com>
2020-03-29 21:13:45 -0300

    Update linux-essentials.sh

--- linux-essentials.sh
+++ linux-essentials.sh
@@ -26,7 +26,7 @@ sudo apt-get install -y chromium-browser
 echo "Instalar Spotify"
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt-get install -y spotify-client
 
 echo "Instalar Git"
 sudo apt-get install -y git
